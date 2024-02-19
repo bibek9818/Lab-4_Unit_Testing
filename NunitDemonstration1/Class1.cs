edfa6818fb2f12ef41a5599c68654ff71e82758f
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-02-18 22:00:46 -0500

    Add project files.

---
+++ NunitDemonstration1/Class1.cs
@@ -0,0 +1,24 @@
+﻿
+namespace NUnitDemonstration1
+{
+
+
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            string result;
+
+            // Mutated code where == is replaced by !=)
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ TestClass/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitDemonstration1;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TriangleTest
+{
+
+    //Bibek Rai
+    public class TraingleTest
+    {
+        [TestFixture]
+        public class TriangleTest1
+        {
+            [Test]
+
+            public void ValidTriangle1()
+            {
+
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);
+            }
+        }
+    }
+
+}
